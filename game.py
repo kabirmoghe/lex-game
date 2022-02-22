6975b0c08aca864df5bb91710b0be6e389878260
kabirmoghe <kabirmoghe@gmail.com>
2022-02-21 19:46:11 -0500

    added tile keys

--- game.py
+++ game.py
@@ -22,7 +22,7 @@ def create_word_info():
     dictionary["Length Order"] = ordered_words
     
     letter_bank = {"a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4, "g": 2, "h": 4, "i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3, "q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8, "y": 4, "z": 10}
-    picked_letters = random.sample(list(letter_bank), 10)
+    picked_letters = random.sample(list(letter_bank), 8)
     
     # Ensures that there are vowels in the picked letters. If not, replaces random letter(s) with a vowel, calculates letter points
     vowels = ["a", "e", "i", "o", "u"]
