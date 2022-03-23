5f5bebc1f927681325d74d1ee4fd2b204e8b58d6
kabirmoghe <kabirmoghe@gmail.com>
2022-03-22 22:21:00 -0400

    altered letter selection technique

--- game.py
+++ game.py
@@ -22,7 +22,48 @@ def create_word_info():
     dictionary["Length Order"] = ordered_words
     
     letter_bank = {"a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4, "g": 2, "h": 4, "i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3, "q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8, "y": 4, "z": 10}
-    picked_letters = random.sample(list(letter_bank), 8)
+    
+    # Choses letters based on frequencies
+    
+    freqs = [("e", 11.1607), ("m", 3.0129),
+            ("a", 8.4966), ("h", 3.0034),
+            ("r", 7.5809), ("g", 2.4705),
+            ("i", 7.5448), ("b", 2.0720),
+            ("o", 7.1635), ("f", 1.8121),
+            ("t", 6.9509), ("y", 1.7779),
+            ("n", 6.6544), ("w", 1.2899),
+            ("s", 5.7351), ("k", 1.1016),
+            ("l", 5.4893), ("v", 1.0074),
+            ("c", 4.5388), ("x", 0.2902),
+            ("u", 3.6308), ("z", 0.2722),
+            ("d", 3.3844), ("j", 0.1965),
+            ("p", 3.1671), ("q", 0.1962)]
+    
+    freqData = pd.DataFrame(freqs, columns = ["letter", "freq"])
+    freqData["num100"] = round(freqData["freq"]*10)
+
+    letterList = []
+
+    for i in range(len(freqData)):
+        letter = freqData.iloc[i]["letter"]
+        freq = int(freqData.iloc[i]["num100"])
+
+        for i in range(freq):
+            letterList.append(letter)
+
+    picked_letters = []
+
+    for i in range(8):
+
+        letter = random.choice(letterList)
+
+        while letter in picked_letters:
+            letter = random.choice(letterList)
+
+        picked_letters.append(letter)
+
+
+    #picked_letters = random.sample(list(letter_bank), 8)
     
     # Ensures that there are vowels in the picked letters. If not, replaces random letter(s) with a vowel, calculates letter points
     vowels = ["a", "e", "i", "o", "u"]
